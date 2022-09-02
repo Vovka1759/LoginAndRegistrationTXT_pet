3e697c3bfdbf4a10b01653f80fadeb28b24f461b
Vovka1759 <vovka1759@gmail.com>
2022-09-02 19:56:38 +0300

    add not full registration

--- LoginAndRegistrationTXT_pet/LoginAndRegistrationTXT_pet.cpp
+++ LoginAndRegistrationTXT_pet/LoginAndRegistrationTXT_pet.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <fstream>
 
 int main()
 {
     std::cout << "Choose option:\n1.Login\n2.Registration\n[1/2]: ";
     int choice;
-    while (true) {
+    bool flag = true;
+    while (flag) {
         std::cin >> choice;
         if (choice == 1 or choice == 2) {
-            break;
+            std::cout << "ddd";
+            flag = false;
         }
         std::cout << "You chose the wrong number! Try again [1/2]: ";
     }
-    std::cout << choice;
+    std::cout << "dddd";
+    switch (choice) {
+
+        case(1):
+
+
+
+            break;
+
+        case(2):
+            std::cout << "ddd";
+            std::ofstream myfile("test.txt");
+            myfile << "Asd";
+            std::cout << "File was created";
+            myfile.close();
+
+            break;
+
+    }
+    
+    return 0;
 }
 
