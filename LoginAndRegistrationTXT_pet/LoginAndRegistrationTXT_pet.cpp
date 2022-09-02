ce6d5f00eff7401d00999e932126645954d0dbf4
Vovka1759 <vovka1759@gmail.com>
2022-09-02 15:22:31 +0300

    add start message, getting choise login/registration

--- LoginAndRegistrationTXT_pet/LoginAndRegistrationTXT_pet.cpp
+++ LoginAndRegistrationTXT_pet/LoginAndRegistrationTXT_pet.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-    std::cout << "Hello World!";
+    std::cout << "Choose option:\n1.Login\n2.Registration\n[1/2]: ";
+    int choise;
+    while (true) {
+        std::cin >> choise;
+        if (choise == 1 or choise == 2) {
+            break;
+        }
+        std::cout << "You chose the wrong number! Try again [1/2]: ";
+    }
+    std::cout << choise;
 }
 
